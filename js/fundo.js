c81ed8cb6cf07c7fa5040fd08af5ad3bcab53f23
LaraYuka <larayuka.sak@gmail.com>
2022-06-28 08:36:45 -0300

    fundo

--- js/fundo.js
+++ js/fundo.js
@@ -3,14 +3,17 @@ $(document).ready(function() {
     $('.escuro').click(function(e) {
         e.preventDefault()
 
-        $('.navbar').removeClass('bg-white')
-        $('.navbar').addClass('bg-dark')
+        $('.navbar').removeClass('bg-dark')
+        $('.navbar').addClass('bg-white')
 
-        $('.nav-link').removeClass('text-dark')
-        $('.nav-link').addClass('text-white')
+        $('.nav-link').removeClass('text-white')
+        $('.nav-link').addClass('text-dark')
 
         $('body').removeClass('bg-white')
         $('body').addClass('bg-dark')
+
+        $('.btn-menu').removeClass('bg-white')
+        $('.btn-menu').addClass('bg-dark')
 
     })
 
@@ -18,14 +21,17 @@ $(document).ready(function() {
         e.preventDefault()
 
 
-        $('.navbar').removeClass('bg-dark')
-        $('navbar').addClass('bg-white')
+        $('.navbar').removeClass('bg-white')
+        $('.navbar').addClass('bg-dark')
 
-        $('.nav-link').removeClass('text-white')
-        $('.nav-link').addClass('text-dark')
+        $('.nav-link').removeClass('text-dark')
+        $('.nav-link').addClass('text-white')
 
         $('body').removeClass('bg-dark')
         $('body').addClass('bg-white')
+
+        $('.btn-menu').removeClass('bg-dark')
+        $('.btn-menu').addClass('bg-white')
     })
 
 
